f9bd72cc7d442a10548391abed311e95ca20409f
tangatgn11 <974099950@qq.com>
2021-05-29 15:20:33 +0800

    design: remove cv2

--- aa.py
+++ aa.py
@@ -3,31 +3,34 @@ import os, uuid, matplotlib
 # matplotlib.use('Agg')
 import matplotlib.pyplot
 import pandas
-import cv2
+import matplotlib.image as mpimg
 import sys
 import numpy as np
 from PIL import Image
 from pyheatmap.heatmap import HeatMap
-
+import matplotlib.pyplot as plt
 
 
 def apply_heatmap(image,data):
     '''image是原图，data是坐标'''
-    image1=cv2.imread(image)
+    image1=mpimg.imread(image)
     background = Image.new("RGB", (image1.shape[1], image1.shape[0]), color=0)
     # 开始绘制热度图
     hm = HeatMap(data)
-    hit_img = hm.heatmap(base=background, r = 30) # background为背景图片，r是半径，默认为10
-    hit_img = cv2.cvtColor(np.asarray(hit_img),cv2.COLOR_RGB2BGR)#Image格式转换成cv2格式
-    overlay = image1.copy()
+    hit_img = hm.heatmap(base=background, r=30)  # background为背景图片，r是半径，默认为10
     alpha = 0.5 # 设置覆盖图片的透明度
-    cv2.rectangle(overlay, (0, 0), (image1.shape[1], image1.shape[0]), (255, 0, 0), -1) # 设置蓝色为热度图基本色蓝色
-    image2 = cv2.addWeighted(overlay, alpha, image1, 1-alpha, 0) # 将背景热度图覆盖到原图
-    image3 = cv2.addWeighted(hit_img, alpha, image2, 1-alpha, 0) # 将热度图覆盖到原图
-    cv2.imshow('ru',image3)
-    cv2.imwrite('test.jpg',image3)
-    cv2.waitKey(0)
-    return image3
+    overlay = np.full(image1.shape, (0, 0, 255)) # 设置蓝色为热度图基本色蓝色
+    image2=overlay*alpha + image1*(1-alpha) # 将背景热度图覆盖到原图
+    image3= np.asarray(hit_img)*alpha + image2* (1-alpha) # 将热度图覆盖到原图
+
+    plt.imshow(np.asarray(image3,dtype=int))
+
+    plt.show()
+
+    # cv2.imshow('ru',image3)
+    # cv2.imwrite('test.jpg',image3)
+    # cv2.waitKey(0)
+    # return image3
 
 
 
